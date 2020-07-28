39a1a3f4ba5f491d107bdcd27c9160bb8b3ca7c3
a3qz <rmichalec14@gmail.com>
2020-07-27 22:33:41 -0700

    more

--- unintellisense/src/extension.ts
+++ unintellisense/src/extension.ts
@@ -1,10 +1,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {adjectives, nouns} from './words';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -13,15 +14,22 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('unintellisense.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerTextEditorCommand('unintellisense.randomVariableName', (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: any[]) => {
+		console.log(context.globalStoragePath);
+		try{
+			var adjective: string = adjectives[Math.floor(Math.random() * (adjectives.length - 0 + 1) + 0)];
+			var noun: string = nouns[Math.floor(Math.random() * (nouns.length - 0 + 1) + 0)];
+			
+		edit.insert(textEditor.selection.active, adjective + noun.replace(/^\w/, (c: string) => c.toUpperCase()))
+		} catch (x){
+			console.log(x);
+		}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Unintellisense!');
+		
 	});
-
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
